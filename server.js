3caaa8846ac9dad1335f118150a8f0df22ff6e3a
Jason H <jahenry@gwmail.gwu.edu>
2017-08-06 18:37:52 -0400

    Final submission update

--- server.js
+++ server.js
@@ -19,66 +19,68 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // friends (DATA)
 // =============================================================
 
-var friends = [{
-    name: "Jason",
-    email: "jason@gmail.com",
+var friends = [
+{
+    name: "George Wilkinson",
+    email: "Wilkinson@gmail.com",
     image: "https://www.fillmurray.com/g/200/300",
     scores: [1,2,3,4,5,4,3,2,1,2]
     },
     {
-    name: "Jeff",
-    email: "jason@gmail.com",
+    name: "Jeff Bridges",
+    email: "Bridges@gmail.com",
     image: "https://www.fillmurray.com/200/300",
     scores: [2,3,4,3,4,3,2,2,2,2]
     },
     {
-    name: "Jaron",
-    email: "jason@gmail.com",
+    name: "Jaron Williams",
+    email: "Williams@gmail.com",
     image: "http://www.stevensegallery.com/g/200/300",
     scores: [3,3,3,4,4,4,3,3,3,3]
     },
     {
-    name: "Burt",
-    email: "jason@gmail.com",
+    name: "Burt Reynolds",
+    email: "Reynolds@gmail.com",
     image: "http://www.stevensegallery.com/200/300",
     scores: [3,3,3,3,3,3,3,3,3,3]
     },
     {
-    name: "Jackson",
-    email: "jason@gmail.com",
+    name: "Jackson Hewitt",
+    email: "Hewitt@gmail.com",
     image: "http://www.placecage.com/200/300",
     scores: [4,4,4,4,4,4,4,4,4,4]
     },
     {
-    name: "Jules",
-    email: "jason@gmail.com",
+    name: "Jules Vern",
+    email: "Vern@gmail.com",
     image: "http://www.placecage.com/gif/200/300",
     scores: [5,5,5,5,5,5,5,5,5,5]
     },
     {
-    name: "Almondo",
-    email: "jason@gmail.com",
+    name: "Almondo Almondstein",
+    email: "Almondstein@gmail.com",
     image: "http://www.placecage.com/g/200/300",
     scores: [4,3,5,5,5,4,3,2,3,4]
     },
     {
-    name: "River",
-    email: "jason@gmail.com",
+    name: "River Banks",
+    email: "Banks@gmail.com",
     image: "http://www.placecage.com/c/200/300",
     scores: [3,4,2,2,3,5,1,2,3,3]
     },
     {
-    name: "Best Match",
-    email: "jason@gmail.com",
+    name: "Award Winner",
+    email: "Winner@gmail.com",
     image: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
     scores: [2,2,2,2,2,2,2,2,2,2]
     },
     {
-    name: "Basin",
-    email: "jason@gmail.com",
+    name: "Basin Hollow",
+    email: "Hollow@gmail.com",
     image: "http://www.placecage.com/c/200/300",
     scores: [4,3,2,5,4,3,1,3,4,5]
-    }];
+    }
+];
 
 // Routes
 // =============================================================
@@ -158,6 +160,7 @@ function fnCompareScores(friends, newFriend){
       }
       else{
         if(QDiffTotal < QDiffLast){
+          QDiffLast = QDiffTotal;
           idxBestMatch = i;
         }
       }
